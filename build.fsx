9992989e1527c4e776598b336524e627cdd616ef
Robin Munn <rmunn@pobox.com>
2016-08-01 18:15:58 +0700

    Output client secret during build
    Just to check if it's working. It is.

--- build.fsx
+++ build.fsx
@@ -16,9 +16,27 @@ let appReferences  =
 // version info
 let version = "0.1"  // or retrieve from CI server
 
+let secretsDir = "./secrets"
+
+let readSecrets servicename =
+    let clientIdFname = sprintf "%s-client-id.txt" servicename
+    let clientSecretFname = sprintf "%s-client-secret.txt" servicename
+    let clientId = ReadLine (secretsDir </> clientIdFname)
+    let clientSecret = ReadLine (secretsDir </> clientSecretFname)
+    (clientId, clientSecret)
+
+let tellSecrets files =
+    let githubId, githubSecret = readSecrets "github"
+    printfn "Github client ID: %s" githubId 
+
 // Targets
 Target "Clean" (fun _ ->
     CleanDirs [buildDir; deployDir]
+)
+
+Target "Tattle" (fun _ ->
+    !! "/**/*.*"
+        |> tellSecrets
 )
 
 Target "Build" (fun _ ->
@@ -35,6 +53,7 @@ Target "Deploy" (fun _ ->
 
 // Build order
 "Clean"
+  ==> "Tattle"
   ==> "Build"
   ==> "Deploy"
 
